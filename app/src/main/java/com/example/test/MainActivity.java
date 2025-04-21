a3443eb8e07ac3eb22d4fcd9afcf17604ae525fe
Edi <alibahramimatin@gmail.com>
2025-04-22 02:22:27 +0330

    one

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -1,74 +1,73 @@
 package com.example.test;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+
+import android.widget.Toast;
+
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText number1, number2;
-    Button calculateBtn;
-    TextView result;
-    private SharedPreferences preferences;
-    private SharedPreferences.Editor editor;
+    Button btnClose,btn1,btn2,btn3,btn4,btn5,
+            btn6,btn7,btn8,btn9,btn0,btnPlus,
+            btnMinus,btnMultiplication,btnDivision,btnCls,btnEqual,btn;
+
+    EditText ET1;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        number1 = findViewById(R.id.number1);
-        number2 = findViewById(R.id.number2);
-        calculateBtn = findViewById(R.id.calculateBtn);
-        result = findViewById(R.id.result);
+            btnClose = findViewById(R.id.btnClose);
+            btn1 = findViewById(R.id.num1);
+            btn2 = findViewById(R.id.num2);
+            btn3 = findViewById(R.id.num3);
+            btn4 = findViewById(R.id.num4);
+            btn5 = findViewById(R.id.num5);
+            btn6 = findViewById(R.id.num6);
+            btn7 = findViewById(R.id.num7);
+            btn8 = findViewById(R.id.num8);
+            btn9 = findViewById(R.id.num9);
+            btn0 = findViewById(R.id.num0);
+            btnPlus = findViewById(R.id.numPlus);
+            btnMinus = findViewById(R.id.numMinus);
+            btnMultiplication = findViewById(R.id.numMultiplication);
+            btnDivision = findViewById(R.id.numDivision);
+            btnCls = findViewById(R.id.numCls);
+            btnEqual = findViewById(R.id.numEqual);
 
-        preferences = getApplicationContext().getSharedPreferences("calcPrefs", Context.MODE_PRIVATE);
-        editor = preferences.edit();
+            ET1 = findViewById(R.id.ET1);
 
-        String lastResult = preferences.getString("result", "no data");
-        result.setText(" the last result : \n " + lastResult);
-
-        calculateBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String num1Str = number1.getText().toString();
-                String num2Str = number2.getText().toString();
-
-                if (!num1Str.isEmpty() && !num2Str.isEmpty()) {
-                    double num1 = Double.parseDouble(num1Str);
-                    double num2 = Double.parseDouble(num2Str);
-
-                    double sum = num1 + num2;
-                    double sub = num1 - num2;
-                    double mul = num1 * num2;
-                    String div;
-                    if (num2 != 0) {
-                        div = String.valueOf(num1 / num2);
-                    } else {
-                        div = "cant divide by zero";
-                    }
-
-
-                    String resultText =
-                            "sum is : " + sum +
-                            "\n minus is : " + sub +
-                            "\n multiplication is : " + mul +
-                            "\n division is : " + div;
-
-                    result.setText(" result: \n " + resultText);
-
-                    editor.putString("result", resultText);
-                    editor.apply();
-                } else {
-                    result.setText("please enter num 1 & 2");
+            btnClose.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    finish();
                 }
-            }
-        });
+            });
+
+
+
+
+
+
+
+
     }
+
+
+
+
 }
+
